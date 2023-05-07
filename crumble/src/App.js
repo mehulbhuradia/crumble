f8b0d7b2d029e1c61e590db6942c936f0648de96
Bieltvedt <isakbieltvedt@gmail.com>
2023-05-08 01:31:25 +0200

    kinda working

--- crumble/src/App.js
+++ crumble/src/App.js
@@ -26,14 +26,16 @@ function Crumble() {
     }
   };
 
+  const handleTyping = async (e) => {setTextInput(e.target.value)}
+
   const prependInputForm = (content) => {
-    return `
+    return (
     <div className ="flex">
       <div className="flex space-x-2">
         <input
           type="text"
           value="${textInput}"
-          onChange="(e) => setTextInput(e.target.value)"
+          onChange="{handleTyping}"
           placeholder="Enter some text"
           className="px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm"
         />
@@ -46,7 +48,7 @@ function Crumble() {
       </div>
       ${content}
     </div>
-    `;
+    );
   };
 
   const html = (
