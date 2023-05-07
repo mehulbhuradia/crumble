be6e2c90525e6541494c51d29e30c0d378442945
Bieltvedt <isakbieltvedt@gmail.com>
2023-05-08 01:13:34 +0200

    init

--- crumble/src/App.js
+++ crumble/src/App.js
@@ -20,29 +20,38 @@ function Crumble() {
       );
 
       console.log("OpenAI Response:", response.data);
-      setPageContent(response.data);
+      setPageContent(prependInputForm(response.data));
     } catch (error) {
       console.error("Error in API call:", error);
     }
   };
 
-  const html = (
-    <div className="space-y-4" ref={pageRef}>
+  const prependInputForm = (content) => {
+    return `
+    <div className ="flex">
       <div className="flex space-x-2">
         <input
           type="text"
-          value={textInput}
-          onChange={(e) => setTextInput(e.target.value)}
+          value="${textInput}"
+          onChange="(e) => setTextInput(e.target.value)"
           placeholder="Enter some text"
           className="px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm"
         />
         <button
-          onClick={handleTextSubmit}
+          onClick="${handleTextSubmit}"
           className="inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
           Submit
         </button>
       </div>
+      ${content}
+    </div>
+    `;
+  };
+
+  const html = (
+    <div className="space-y-4" ref={pageRef}>
+      {prependInputForm("")}
     </div>
   );
 
@@ -58,3 +67,4 @@ function Crumble() {
 }
 
 export default Crumble;
+
--- skynet/app.py
+++ skynet/app.py
@@ -30,7 +30,8 @@ async def magic(input_data: MagicInput):
     pageContent = input_data.pageContent
     try:
         response = openai.Completion.create(
-            model="text-davinci-003",
+            # model="text-davinci-003",
+            model = "gpt-4",
             prompt=f"Use the following text input: {textInput}\n\nBased on the input, modify and style the HTML below:\n\n{pageContent}\n\nMake sure to keep any existing components unless explicitly mentioned. Here are some specific instructions to follow:\n\n- If the text input contains a specific keyword or phrase, add a new section to the HTML with relevant content.\n- If the text input mentions a specific color or font, change the style of the relevant section(s) in the HTML to match.\n- If the text input suggests a change to an existing component, modify that component as necessary.\n- If the text input suggests removing a component, explicitly state which component(s) should be removed.\n\nBe creative and use your best judgement to create a visually appealing and functional page based on the user's input.",
             max_tokens=1024,
             n=1,
